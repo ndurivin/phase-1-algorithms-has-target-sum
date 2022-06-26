b0bf5205fcf8cab4b8200facd3daa4d2cf86ad19
Vinn Nduri <ndurivo@gmail.com>
2022-06-26 07:08:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    
+    const num1 = target - array[i];
+    //loop through the rest of the array 
+    for (let j = i + 1; j < array.length; j++) {
+
+      //Check if total matches target
+      if (array[j] === num1) {
+        return true
+      } 
+
+    }
+  } 
+  return false
 }
+
+console.log(hasTargetSum([3,8,12,4,11,7],10));
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log( hasTargetSum([1, 2, 5], 4));
 
 /* 
   Write the Big O time complexity of your function here
@@ -28,7 +47,19 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 4));
+  console.log("");
+
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-8 ,6, 7, 5, 20], 25));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([6, 10, 5, 6, 30], 11));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
